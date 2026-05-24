0508106dc9367f7edabf224caa6f3aa741babdaa
unknown <ayualdafuadiyah123@gmail.com>
2026-05-24 21:46:33 +0700

    update banyak

--- resources/views/component/c_dashboard/sidebar/sidebar_user.blade.php
+++ resources/views/component/c_dashboard/sidebar/sidebar_user.blade.php
@@ -66,7 +66,7 @@
                     <circle cx="12" cy="12" r="10" />
                     <polyline points="12 6 12 12 16 14" />
                 </svg>
-                <span>History Booking</span>
+                <span>Riwayat Pesanan</span>
             </a>
         </nav>
     </div>
